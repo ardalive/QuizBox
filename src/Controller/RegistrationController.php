360e158dc3b4f33aa477350ea715543510c48317
Victor Yurchyk <rikartov@gmail.com>
2020-09-03 20:52:15 +0300

    registration changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,6 +6,11 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +25,28 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+//        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createFormBuilder($user, [
+            'attr'=>['class'=>'form-signup']
+        ])
+            ->add('email', EmailType::class, [
+                'label'=> false,
+                'attr'=>['class'=>'form-control']
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => [],
+                'second_options' => [],
+                'options' => ['label' => false,'attr' => ['class' => 'form-control']],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Register',
+                'attr'=>['class'=>'btn btn-lg btn-primary']
+            ])
+            ->getForm();
+        ;
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -28,7 +54,8 @@ class RegistrationController extends AbstractController
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+//                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
 
