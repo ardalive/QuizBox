e3c213b49bcb3abc9e704c94fbd583bff66e49a1
SERgey1312 <serg.vishnev.1992@gmail,com>
2020-09-05 11:36:29 +0300

    'new_field_in_entity_User'

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,6 +36,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=70)
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -113,4 +118,17 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
 }
