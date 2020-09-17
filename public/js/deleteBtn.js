40dfa6bd51502fd32422ef1140bd2f141a4c7e26
SERgey1312 <serg.vishnev.1992@gmail,com>
2020-09-17 17:04:15 +0300

    delete quiz done

--- public/js/deleteBtn.js
+++ public/js/deleteBtn.js
@@ -1,4 +1,20 @@
 let deleteBtn = document.getElementById('deleteBtnLink');
 deleteBtn.addEventListener('click', function () {
-    //TODO
+    let content = document.getElementById('content');
+    content.style.opacity = 0.1;
+
+    let header = document.getElementsByClassName('navbar')[0];
+    header.style.opacity = 0;
+    header.style.position = 'absolute';
+
+    let footer = document.getElementsByTagName('footer')[0];
+    footer.style.opacity = 0.1;
+
+    let window_box = document.getElementById('window');
+    window_box.style.display = 'block';
+    setTimeout(()=> window_box.style.opacity = 1, 0);
+
+    let blackSquare = document.getElementById('blackSquare');
+    blackSquare.style.display = 'block';
+
 })
--- src/Controller/DeleteQuizController.php
+++ src/Controller/DeleteQuizController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Quiz;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +12,22 @@ class DeleteQuizController extends AbstractController
     /**
      * @Route("admin/quiz/delete{id}", name="delete_quiz")
      */
-    public function index(int $id)
+    public function index(int $id, EntityManagerInterface $entityManager)
     {
+        $oneQuiz = $entityManager->getRepository(Quiz::class)->find($id);
+
+        if (!$oneQuiz) {
+            throw $this->createNotFoundException(
+                'This quiz does not exists'
+            );
+        }
+
+        $entityManager->remove($oneQuiz);
+        $entityManager->flush();
+
         return $this->render('delete_quiz/index.html.twig', [
             'controller_name' => 'DeleteQuizController',
+            'id' => $id,
         ]);
     }
 }
