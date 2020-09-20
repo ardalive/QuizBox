5411f4f44f2efbb8f7103a5cb9b3098f5f87ce6b
SERgey1312 <serg.vishnev.1992@gmail,com>
2020-09-21 02:27:34 +0300

    filter and sort in quiz table

--- src/Controller/QuestionPageController.php
+++ src/Controller/QuestionPageController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Questions;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionPageController extends AbstractController
@@ -12,19 +14,28 @@ class QuestionPageController extends AbstractController
     /**
      * @Route("/admin/questions", name="question_page")
      */
-    public function index(EntityManagerInterface $entityManager)
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request)
     {
-        $allQuestion = $entityManager->getRepository(Questions::class)->findAll();
+//        $allQuestion = $entityManager->getRepository(Questions::class)->findAll();
+//
+//        if (!$allQuestion) {
+//            throw $this->createNotFoundException(
+//                'Not found :('
+//            );
+//        }
 
-        if (!$allQuestion) {
-            throw $this->createNotFoundException(
-                'Not found :('
-            );
-        }
+        $dql   = "SELECT a FROM App:Questions a";
+        $query = $entityManager->createQuery($dql);
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
 
         return $this->render('question_page/index.html.twig', [
-            'allQuestion' => $allQuestion,
-
+           // 'allQuestion' => $allQuestion,
+            'pagination' => $pagination,
         ]);
     }
 }
